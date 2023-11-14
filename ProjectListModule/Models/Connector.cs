99b4dabdf033c59a38316a77252685eee029c67c
Akn <sibata486@gmail.com>
2023-11-14 20:33:48 +0900

    add / アプリ起動 - プロジェクト一覧読み込み　までを実装

---
+++ ProjectListModule/Models/Connector.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using YouTrackSharp;
+using YouTrackSharp.Projects;
+
+namespace ProjectListModule.Models
+{
+    public class Connector
+    {
+        public Connector(string url, string token)
+        {
+            Connection = new BearerTokenConnection(url, token);
+        }
+
+        public List<Project> Projects { get; private set; }
+
+        private BearerTokenConnection Connection { get; set; }
+
+        public async Task LoadProjects()
+        {
+            try
+            {
+                var projectsService = Connection.CreateProjectsService();
+                var projects = await projectsService.GetAccessibleProjects();
+                Projects = projects.ToList();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e}(Connector : 46)");
+                throw;
+            }
+        }
+    }
+}
--- ProjectListModule/ViewModels/ViewAViewModel.cs
+++ ProjectListModule/ViewModels/ViewAViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Prism.Mvvm;
+using ProjectListModule.Models;
 using YouTrackSharp.Projects;
 
 namespace ProjectListModule.ViewModels
@@ -7,6 +11,28 @@ namespace ProjectListModule.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class ViewAViewModel : BindableBase
     {
-        public List<Project> Projects { get; set; }
+        private List<Project> projects;
+
+        public List<Project> Projects { get => projects; private set => SetProperty(ref projects, value); }
+
+        public ViewAViewModel()
+        {
+            var uri = File.ReadAllText(
+                    $@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\youtrackInfo\uri.txt")
+                .Replace("\n", "");
+
+            var perm = File.ReadAllText(
+                    $@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\youtrackInfo\perm.txt")
+                .Replace("\n", "");
+
+            _ = GetProjectsAsync(uri, perm);
+        }
+
+        private async Task GetProjectsAsync(string uri, string perm)
+        {
+            var connector = new Connector(uri, perm);
+            await connector.LoadProjects();
+            Projects = connector.Projects;
+        }
     }
 }
