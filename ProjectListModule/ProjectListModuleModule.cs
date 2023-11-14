aa2dfbb4754b61c2919f9159b172dde8239e200e
Akn <sibata486@gmail.com>
2023-11-14 19:59:09 +0900

    add / MainWindow に ProjectListModule が表示されるようにした。

--- ProjectListModule/ProjectListModuleModule.cs
+++ ProjectListModule/ProjectListModuleModule.cs
@@ -1,5 +1,7 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
+using ProjectListModule.Views;
 
 namespace ProjectListModule
 {
@@ -7,6 +9,8 @@ namespace ProjectListModule
     {
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            var regionMan = containerProvider.Resolve<IRegionManager>();
+            regionMan.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
