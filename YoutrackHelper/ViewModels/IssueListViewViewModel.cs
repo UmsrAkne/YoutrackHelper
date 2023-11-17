6b996ce50f280ff97366a97efd1dc11210b6e42a
Akn <sibata486@gmail.com>
2023-11-17 22:39:31 +0900

    add / 課題一覧を表示するためのビューとモジュールを追加

---
+++ YoutrackHelper/ViewModels/IssueListViewViewModel.cs
@@ -0,0 +1,9 @@
+using Prism.Mvvm;
+
+namespace YoutrackHelper.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class IssueListViewViewModel : BindableBase
+    {
+    }
+}
---
+++ YoutrackHelper/Views/IssueListView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace YoutrackHelper.Views
+{
+    public partial class IssueListView : UserControl
+    {
+        public IssueListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
