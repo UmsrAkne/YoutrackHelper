62078903b7da7fb404655241878b86b69b81a738
Akn <sibata486@gmail.com>
2024-02-13 23:05:34 +0900

    add ui / 登録済みの作業時間の合計をビューに表示

---
+++ YoutrackHelper/Models/TimeSpanConverterEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YoutrackHelper.Models
+{
+    public class TimeSpanConverterEx : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var ts = (TimeSpan)value;
+                return ts != TimeSpan.Zero
+                    ? $"{(int)ts.TotalDays} day {ts:hh\\:mm}"
+                    : string.Empty;
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
